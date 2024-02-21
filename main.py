76c0e005ba1aece260e5a1a69900b2da323c88e6
bhavana1111 <bhavanakondeti2000@gmail.com>
2024-02-21 17:39:16 -0600

    Scarping list of movies and creating scripts for them in different files

---
+++ main.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+#Fetch the page(obtain a response object)
+#page content
+#content=response.text
+#Create Soup
+#Soup=BeautifulSoup(content,"lxml") here lxml is a parser
+root='https://subslikescript.com'
+website=f'{root}/movies'
+response=requests.get(website)
+content=response.text
+soup=BeautifulSoup(content,'lxml')
+
+box=soup.find("article",class_="main-article")
+
+
+links=[]
+
+for link in box.find_all('a',href=True):
+    links.append(link['href'])
+
+print(links)
+
+for link in links:
+    website=f'{root}/{link}'
+    response=requests.get(website)
+    content=response.text
+    soup=BeautifulSoup(content,'lxml')
+    box=soup.find("article",class_="main-article")
+    title=box.find('h1').get_text()
+    transcript=box.find('div',class_="full-script").get_text(strip=True,separator=' ')
+
+
+    with open(f'{title}.txt','w',encoding="utf-8") as file:
+        file.write(transcript)
